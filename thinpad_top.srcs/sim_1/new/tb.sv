804c0f57f5a95d6bf537099d0a6491041ab254d8
郭昊 <guo-h19@mails.tsinghua.edu.cn>
2021-11-04 22:52:05 +0800

    a low freq ver.

--- thinpad_top.srcs/sim_1/new/tb.sv
+++ thinpad_top.srcs/sim_1/new/tb.sv
@@ -46,7 +46,7 @@ wire uart_tbre;          //发送数据标志
 wire uart_tsre;          //数据发送完毕标志
 
 //Windows需要注意路径分隔符的转义，例如"D:\\foo\\bar.bin"
-parameter BASE_RAM_INIT_FILE = "C:\\Computer-Organization\\rv\\pipeline\\stage6.bin"; //BaseRAM初始化文件，请修改为实际的绝对路径
+parameter BASE_RAM_INIT_FILE = "C:\\Computer-Organization\\rv\\kernel1.bin"; //BaseRAM初始化文件，请修改为实际的绝对路径
 parameter EXT_RAM_INIT_FILE = "/tmp/eram.bin";    //ExtRAM初始化文件，请修改为实际的绝对路径
 parameter FLASH_INIT_FILE = "/tmp/kernel.elf";    //Flash初始化文件，请修改为实际的绝对路径
 
@@ -59,13 +59,13 @@ initial begin
     reset_btn = 0;
 
     // 模拟PC通过串口发送字符
-    cpld.pc_send_byte(8'h72);
-    cpld.pc_send_byte(8'h64);
-    cpld.pc_send_byte(8'h80);
-    cpld.pc_send_byte(8'h00);
-    cpld.pc_send_byte(8'h00);
-    cpld.pc_send_byte(8'h00);
-    cpld.pc_send_byte(8'h80);
+    //cpld.pc_send_byte(8'h72);
+    //cpld.pc_send_byte(8'h64);
+    //cpld.pc_send_byte(8'h80);
+    //cpld.pc_send_byte(8'h00);
+    //cpld.pc_send_byte(8'h00);
+    //cpld.pc_send_byte(8'h00);
+    //cpld.pc_send_byte(8'h80);
 end
 
 // 待测试用户设计
--- thinpad_top.srcs/sources_1/new/mem.sv
+++ thinpad_top.srcs/sources_1/new/mem.sv
@@ -40,6 +40,8 @@ reg[8:0] ram_enable;
 reg[31:0] cur_addr;
 reg[31:0] cur_data_in;
 reg[31:0] cur_data_out;
+reg[31:0] ram_data;
+reg[3:0] ram_be_n;
 
 localparam IDLE = 9'b111111111;
 localparam READ_BASE = 9'b100111111;
@@ -56,24 +58,22 @@ assign base_ram_data = ((ram_enable == WRITE_BASE) || (ram_enable == WRITE_UART)
 assign ext_ram_data = (ram_enable == WRITE_EXT) ? cur_data_in : 32'bz;
 assign data_out = cur_data_out;
 assign state_out = state;
-assign base_ram_be_n = 4'b0000;
-assign ext_ram_be_n = 4'b0000;
+assign base_ram_be_n = ram_be_n;
+assign ext_ram_be_n = ram_be_n;
 
 reg[3:0] state;
 localparam S_IDLE = 0;
-localparam S_READ_RAM = 1;
-localparam S_WRITE_RAM = 2;
-localparam S_READ_UART = 3;
-localparam S_WRITE_UART = 4;
-localparam S_READ_UART_GAP = 5;
-localparam S_READ_UART_GAP_2 = 6;
-localparam S_WRITE_UART_WAIT_TBRE = 7;
+localparam S_RW_RAM = 1;
+localparam S_READ_UART = 2;
+localparam S_WRITE_UART = 3;
+localparam S_READ_UART_GAP = 4;
+localparam S_READ_UART_GAP_1 = 5;
+localparam S_WRITE_UART_WAIT_TBRE = 6;
+localparam S_WRITE_UART_WAIT_TBRE_1 = 7;
 localparam S_WRITE_UART_WAIT_TSRE = 8;
-localparam S_READ_UART_STATE = 9;
-localparam S_WRITE_RAM_BYTE_READ = 10;
-localparam S_WRITE_RAM_BYTE_WRITE = 11;
-localparam S_DONE = 12;
-
+localparam S_WRITE_UART_WAIT_TSRE_1 = 9;
+localparam S_READ_UART_STATE = 10;
+localparam S_DONE = 11;
 
 assign {
     base_ram_ce_n, base_ram_oe_n, base_ram_we_n, 
@@ -104,164 +104,189 @@ assign ext_byte_data = addr[1:0] == 2'b00 ? ext_ram_data[7:0] :
 
 
 always_comb begin
-    cur_data_out = 32'b0;
-    case (ram_enable)
-        READ_BASE: case (inst[1:0])
-            `MEM_BYTE: cur_data_out = {{24{base_byte_sign}}, base_byte_data};
-            `MEM_WORD: cur_data_out = base_ram_data;
+    ram_data = 32'b0;
+    case ({inst[3:2], addr[28], addr[22]})
+        `MEM_READ_BASE: case (inst[1:0])
+            `MEM_BYTE: ram_data = {{24{base_byte_sign}}, base_byte_data};
+            `MEM_WORD: ram_data = base_ram_data;
             default: begin end
         endcase
-        READ_EXT: case (inst[1:0])
-            `MEM_BYTE: cur_data_out = {{24{ext_byte_sign}}, ext_byte_data};
-            `MEM_WORD: cur_data_out = ext_ram_data;
+        `MEM_READ_EXT: case (inst[1:0])
+            `MEM_BYTE: ram_data = {{24{ext_byte_sign}}, ext_byte_data};
+            `MEM_WORD: ram_data = ext_ram_data;
             default: begin end
         endcase
-        READ_UART: cur_data_out = { 24'b0, base_ram_data[7:0] };
-        READ_UART_STATE: cur_data_out = { 26'b0, uart_tbre && uart_tsre, 4'b0, uart_dataready };
+        `MEM_READ_UART: case (addr[2:0])
+            3'b101: ram_data = { 26'b0, uart_tbre && uart_tsre, 4'b0, uart_dataready }; // state
+            3'b000: ram_data = { 24'b0, base_ram_data[7:0] }; // data
+            default: begin end
+        endcase
         default: begin end
     endcase
 end
 
-assign done = (state == S_DONE || state == S_READ_RAM || state == S_WRITE_RAM) ? 1'b1 : 1'b0;
+assign done = next_state == S_RW_RAM ? 1'b1 : 1'b0;
 assign idle = (state == S_IDLE) ? 1'b1 : 1'b0;
 
-
+// Try to accelerate R&W of memory to 1 cycle.
+reg[3:0] next_state;
+// This logic is only for idle / read_ram / write_ram
+always_comb begin
+    ram_be_n = 4'b0000;
+    ram_enable = IDLE;
+    cur_addr = 32'b0;
+    cur_data_in = data_in;
+    case(state)
+        S_IDLE, S_RW_RAM: begin
+            cur_addr = { 12'b0, addr[21:2] };
+            case ({inst[3:2], addr[28], addr[22]})
+                `MEM_READ_BASE: begin
+                    ram_enable = READ_BASE;
+                    next_state = S_DONE;
+                end
+                `MEM_WRITE_BASE: begin
+                    ram_enable = WRITE_BASE;
+                    next_state = S_DONE;
+                    case (inst[1:0]) 
+                        `MEM_BYTE: begin
+                            case (addr[1:0])
+                                2'b00: begin
+                                    ram_be_n = 4'b1110;
+                                    cur_data_in = {24'b0, data_in[7:0]}; 
+                                end
+                                2'b01: begin
+                                    ram_be_n = 4'b1101;
+                                    cur_data_in = {16'b0, data_in[7:0], 8'b0};
+                                end
+                                2'b10: begin
+                                    ram_be_n = 4'b1011;
+                                    cur_data_in = {8'b0, cur_data_in[7:0], 16'b0};
+                                end
+                                2'b11: begin
+                                    ram_be_n = 4'b0111;
+                                    cur_data_in = {cur_data_in[7:0], 24'b0};  
+                                end
+                            endcase         
+                        end
+                        default: begin end
+                    endcase
+                end
+                `MEM_READ_EXT: begin
+                    ram_enable = READ_EXT;
+                    next_state = S_DONE;
+                end
+                `MEM_WRITE_EXT: begin
+                    ram_enable = WRITE_EXT;
+                    next_state = S_DONE;
+                    case (inst[1:0]) 
+                        `MEM_BYTE: begin
+                            case (addr[1:0])
+                                2'b00: begin
+                                    ram_be_n = 4'b1110;
+                                    cur_data_in = {24'b0, data_in[7:0]}; 
+                                end
+                                2'b01: begin
+                                    ram_be_n = 4'b1101;
+                                    cur_data_in = {16'b0, data_in[7:0], 8'b0};
+                                end
+                                2'b10: begin
+                                    ram_be_n = 4'b1011;
+                                    cur_data_in = {8'b0, cur_data_in[7:0], 16'b0};
+                                end
+                                2'b11: begin
+                                    ram_be_n = 4'b0111;
+                                    cur_data_in = {cur_data_in[7:0], 24'b0};  
+                                end
+                            endcase 
+                        end
+                        default: begin end
+                    endcase
+                end
+                `MEM_READ_UART: begin
+                    case (addr[2:0])
+                        3'b101: begin
+                            ram_enable = READ_UART_STATE;
+                            next_state = S_READ_UART_STATE;
+                        end
+                        3'b000: begin
+                            next_state = S_READ_UART;
+                        end
+                        default: begin
+                            next_state = S_DONE; // ignore other addresses except 0x10000000 and 0x10000005
+                        end
+                    endcase
+                end
+                `MEM_WRITE_UART: begin
+                    case (addr[2:0])
+                        3'b000: begin
+                            ram_enable = WRITE_UART;
+                            next_state = S_WRITE_UART;
+                        end
+                        default: begin
+                            next_state = S_DONE;
+                        end
+                    endcase
+                end
+                default: begin
+                    next_state = S_DONE;        
+                end
+            endcase
+        end
+        S_READ_UART: begin
+            if (uart_dataready == 1) begin
+                next_state = S_READ_UART_GAP;
+            end else begin
+                next_state = S_READ_UART;
+            end
+        end   
+        S_READ_UART_GAP: begin
+            ram_enable = READ_UART; // uart_dataready == 1
+            next_state = S_READ_UART_GAP_1;
+        end
+        S_READ_UART_GAP_1: begin
+            ram_enable = READ_UART;
+            next_state = S_DONE;
+        end
+        S_WRITE_UART: begin
+            ram_enable = WRITE_UART;
+            next_state = S_WRITE_UART_WAIT_TBRE;
+        end
+        S_WRITE_UART_WAIT_TBRE: begin
+            // ram_enable = IDLE;
+            if (uart_tbre == 1) next_state = S_WRITE_UART_WAIT_TBRE_1; 
+            else next_state = S_WRITE_UART_WAIT_TBRE;
+        end
+        S_WRITE_UART_WAIT_TBRE_1: begin
+            next_state = S_WRITE_UART_WAIT_TSRE;
+        end
+        S_WRITE_UART_WAIT_TSRE: begin
+            if (uart_tsre == 1) next_state = S_WRITE_UART_WAIT_TSRE_1;
+            else next_state = S_WRITE_UART_WAIT_TSRE;
+        end
+        S_WRITE_UART_WAIT_TSRE_1: begin
+            next_state = S_DONE;
+        end
+        S_READ_UART_STATE: begin
+            ram_enable = READ_UART_STATE;
+            next_state = S_DONE;
+        end
+        S_DONE: begin
+            ram_enable = IDLE;
+            next_state = S_RW_RAM;
+        end
+        default: next_state = S_IDLE;
+    endcase
+end
 
 always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-        ram_enable <= IDLE;
-        cur_data_in <= 32'b0;
-        cur_addr <= 32'b0;
         state <= S_IDLE;
     end else begin
-        case (state)
-            S_IDLE: begin
-                cur_data_in <= data_in;
-                cur_addr <= { 12'b0, addr[21:2] };
-                case ({inst[3:2], addr[28], addr[22]})
-                    `MEM_READ_BASE: begin
-                        ram_enable <= READ_BASE;
-                        state <= S_READ_RAM;
-                    end
-                    `MEM_WRITE_BASE: begin
-                        case (inst[1:0]) 
-                            `MEM_WORD: begin
-                                ram_enable <= WRITE_BASE;
-                                state <= S_WRITE_RAM;
-                            end
-                            `MEM_BYTE: begin
-                                ram_enable <= READ_BASE; // read because I should read first and replace
-                                state <= S_WRITE_RAM_BYTE_READ;
-                            end
-                            default: begin end
-                        endcase
-                    end
-                    `MEM_READ_EXT: begin
-                        ram_enable <= READ_EXT;
-                        state <= S_READ_RAM;
-                    end
-                    `MEM_WRITE_EXT: begin
-                        case (inst[1:0]) 
-                            `MEM_WORD: begin
-                                ram_enable <= WRITE_EXT;
-                                state <= S_WRITE_RAM;
-                            end
-                            `MEM_BYTE: begin
-                                ram_enable <= READ_EXT; // read because I should read first and replace
-                                state <= S_WRITE_RAM_BYTE_READ;
-                            end
-                            default: begin end
-                        endcase
-                    end
-                    `MEM_READ_UART: begin
-                        case (addr[2:0])
-                            3'b101: begin
-                                ram_enable <= READ_UART_STATE;
-                                state <= S_READ_UART_STATE;
-                            end
-                            3'b000: state <= S_READ_UART;
-                            default: state <= S_DONE; // ignore other addresses except 0x10000000 and 0x10000005
-                        endcase
-                    end
-                    `MEM_WRITE_UART: begin
-                        case (addr[2:0])
-                            3'b000: begin
-                                ram_enable <= WRITE_UART;
-                                state <= S_WRITE_UART;
-                            end
-                            default: state <= S_DONE;
-                        endcase
-                    end
-                    default: begin
-                        ram_enable <= IDLE;
-                        state <= S_DONE; // done -> idle                
-                    end
-                endcase
-            end
-            S_READ_RAM: begin
-                ram_enable <= IDLE;
-                state <= S_IDLE;
-            end
-            S_WRITE_RAM: begin
-                ram_enable <= IDLE;
-                state <= S_IDLE;
-            end
-            S_READ_UART: begin
-                if (uart_dataready == 1) begin
-                    ram_enable <= READ_UART;
-                    state <= S_READ_UART_GAP;
-                end else begin
-                    state <= S_READ_UART;
-                end
-            end        
-            S_READ_UART_GAP: state <= S_READ_UART_GAP_2;
-            S_READ_UART_GAP_2: state <= S_DONE;
-            S_WRITE_UART: begin
-                ram_enable <= IDLE;
-                state <= S_WRITE_UART_WAIT_TBRE;
-            end 
-            S_WRITE_UART_WAIT_TBRE: begin
-                if (uart_tbre == 1) state <= S_WRITE_UART_WAIT_TSRE; 
-                else state <= S_WRITE_UART_WAIT_TBRE;
-            end
-            S_WRITE_UART_WAIT_TSRE: begin
-                if (uart_tsre == 1) state <= S_DONE;
-                else state <= S_WRITE_UART_WAIT_TSRE;
-            end
-            S_READ_UART_STATE: begin
-                state <= S_DONE;
-            end
-            S_WRITE_RAM_BYTE_READ: begin
-                case (ram_enable)
-                    READ_BASE: begin
-                        ram_enable <= WRITE_BASE;
-                        case(addr[1:0])
-                            2'b00: cur_data_in <= {base_ram_data[31:8], cur_data_in[7:0]}; // little endian
-                            2'b01: cur_data_in <= {base_ram_data[31:16], cur_data_in[7:0], base_ram_data[7:0]};
-                            2'b10: cur_data_in <= {base_ram_data[31:24], cur_data_in[7:0], base_ram_data[15:0]};
-                            2'b11: cur_data_in <= {cur_data_in[7:0], base_ram_data[23:0]};                    
-                        endcase
-                    end
-                    READ_EXT: begin
-                        ram_enable <= WRITE_EXT;
-                        case(addr[1:0])
-                            2'b00: cur_data_in <= {ext_ram_data[31:8], cur_data_in[7:0]}; // little endian
-                            2'b01: cur_data_in <= {ext_ram_data[31:16], cur_data_in[7:0], ext_ram_data[7:0]};
-                            2'b10: cur_data_in <= {ext_ram_data[31:24], cur_data_in[7:0], ext_ram_data[15:0]};
-                            2'b11: cur_data_in <= {cur_data_in[7:0], ext_ram_data[23:0]};                    
-                        endcase
-                    end
-                    default: begin end
-                endcase
-                state <= S_WRITE_RAM;
-            end
-            S_DONE: begin
-                ram_enable <= IDLE;
-                state <= S_IDLE;
-            end
-            default: begin end
-        endcase
+        state <= next_state;
+        if (next_state == S_DONE) cur_data_out <= ram_data;
+        else cur_data_out <= 32'bz;
     end
 end
+
     
 endmodule
