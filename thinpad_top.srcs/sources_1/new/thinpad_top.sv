d005ca5b558cb52615a52ee68a3c70e20b8d5cd5
郭昊 <guo-h19@mails.tsinghua.edu.cn>
2021-11-04 11:09:24 +0000

    debugging...

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -348,7 +348,7 @@ always_ff @(posedge clk_50M or posedge reset_btn) begin
                 id_exe_pred_pc <= pred_pc;
                 exe_mem_pred_pc <= id_exe_pred_pc;
                 
-                if (is_jump_op && exe_mem_pred_pc != next_pc) begin
+                if (pc_jumping) begin
                     num_reg <= 8'h01;
                     pc <= next_pc; // pred failed, use next_pc and stop the pipeline
                     reg_inst <= INST_INVALID; 
