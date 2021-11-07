4d40dee44d55df63d83e395656ce9fbb05af9b18
TerryHu <humuyan@163.com>
2021-11-07 17:46:29 +0800

    Resolve a latch

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -120,7 +120,7 @@ mem _mem(
     .inst(mem_inst),
     .addr(mem_addr),
     .data_in(mem_data_in),
-    .clk(clk_11M0592),
+    .clk(clk_50M),
     .rst(reset_btn),
     .done(mem_done),
     .idle(mem_idle),
@@ -171,7 +171,7 @@ wire[31:0] reg_rdata1;
 wire[31:0] reg_rdata2;
 
 regfile _regfile(
-    .clk(clk_11M0592),
+    .clk(clk_50M),
     .rst(reset_btn),
     .we(reg_we),
     .waddr(reg_waddr),
@@ -277,7 +277,7 @@ reg[31:0] pred_pc;
 reg[31:0] id_exe_pred_pc, exe_mem_pred_pc;
 // pred pc
 branch_pred _branch_pred(
-    .clk(clk_11M0592),
+    .clk(clk_50M),
     .is_jump_op(is_jump_op),
     .last_jump_pc(exe_mem_pc[4:2]),
     .last_jump_result(pc_jumping),
@@ -372,7 +372,7 @@ reg[4:0] wb_reg_s, wb_reg_t, wb_reg_d;
 
 localparam INST_INVALID = 32'b0;
 
-always_ff @(posedge clk_11M0592 or posedge reset_btn) begin
+always_ff @(posedge clk_50M or posedge reset_btn) begin
     if (reset_btn) begin
         reg_inst <= INST_INVALID;
         exe_mem_op <= `OP_INVALID;
